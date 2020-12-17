6459b3f78f65dbab482764c0503a2bc73c4d14ac
junaidansari6311 <junaidansari6311@gmail.com>
2020-12-17 07:10:33 +0530

    ADDED : Design for sidebar component

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -4,15 +4,37 @@ import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle'
 import {withRouter} from "react-router";
+import Sidebar from "./Sidebar";
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import IconButton from "@material-ui/core/IconButton";
 
 class Navbar extends Component {
+    constructor() {
+        super();
+        this.state = {
+            isOpen:false,
+        };
+        this.handleSidebar = this.handleSidebar.bind(this);
+    }
+
+    handleSidebar = () => {
+        this.setState(prevState => ({ isOpen: !prevState.isOpen }));
+    }
+
+    handleClickAway = () => {
+        this.setState({ isOpen : false});
+    }
+
+
     render() {
         return (
+            <ClickAwayListener onClickAway={this.handleClickAway}>
             <div className="navbar-container">
                 <div className="navbar-content">
                     <div className="navbar-start">
-                        <div className="menu-icon">
+                        <div className="menu-icon" onClick={this.handleSidebar}>
                             <MenuIcon/>
+                            <Sidebar openSidebar={this.state.isOpen}/>
                         </div>
                         <div className="fundoo-container">
                             <div className="fundoo-logo">Fundoo Notes</div>
@@ -36,6 +58,7 @@ class Navbar extends Component {
                 </div>
 
             </div>
+            </ClickAwayListener>
         );
     }
 }
---
+++ src/component/Sidebar.jsx
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+import '../scss/sidebar.scss';
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
+import Drawer from "@material-ui/core/Drawer";
+import ArchiveIcon from '@material-ui/icons/Archive';
+import ReminderIcon from '@material-ui/icons/Notifications';
+import TrashIcon from '@material-ui/icons/Delete';
+import NoteIcon from '@material-ui/icons/EmojiObjects';
+import EditIcon from '@material-ui/icons/EditOutlined';
+
+class Sidebar extends Component {
+    constructor(props) {
+        super();
+    }
+    render() {
+        return (
+            <Drawer variant='persistent' open={this.props.openSidebar} >
+                <List>
+                    <ListItem button>
+                        <ListItemIcon><NoteIcon/></ListItemIcon>
+                        <ListItemText primary="Note"/>
+                    </ListItem>
+
+                    <ListItem button>
+                        <ListItemIcon><ReminderIcon/></ListItemIcon>
+                        <ListItemText primary="Reminder"/>
+                    </ListItem>
+
+                    <Divider/>
+
+                    <ListItem button>
+                        <ListItemIcon><EditIcon/></ListItemIcon>
+                        <ListItemText primary="Edit Label"/>
+                    </ListItem>
+
+                    <Divider/>
+
+                    <ListItem button>
+                        <ListItemIcon><ArchiveIcon/></ListItemIcon>
+                        <ListItemText primary="Archive"/>
+                    </ListItem>
+
+                    <ListItem button>
+                        <ListItemIcon><TrashIcon/></ListItemIcon>
+                        <ListItemText primary="Trash"/>
+                    </ListItem>
+                </List>
+            </Drawer>
+        );
+    }
+}
+
+export default Sidebar;
